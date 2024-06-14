645332edfe4e9ceea55eea0848bdd0cd45371a95
Alien Brain <murad.dub@gmail.com>
2024-06-15 01:08:20 +0600

    Logged in for User: Authentication Successful

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -9,23 +9,23 @@ import { User } from "../user/user.model";
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user is exist
-  // const user = await User.isUserExistsByCustomEmail(payload.email);
+  const user = await User.isUserExistsByCustomEmail(payload.email);
 
-  // if (!user) {
-  //   throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
-  // }
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
+  }
   // checking if the user is already deleted
 
-  // const isDeleted = user?.isDeleted;
+  const isDeleted = user?.isDeleted;
 
-  // if (isDeleted) {
-  //   throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !");
-  // }
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !");
+  }
 
   //checking if the password is correct
 
-  // if (!(await User.isPasswordMatched(payload?.password, user?.password)))
-  //   throw new AppError(httpStatus.FORBIDDEN, "Password do not matched");
+  if (!(await User.isPasswordMatched(payload?.password, user?.password)))
+    throw new AppError(httpStatus.FORBIDDEN, "Password do not matched");
 
   //create token and sent to the  client
 
